c36a3264b069918462868af78fb8703f7f6f82ac
Hoodoo42 <create@hoodooverse.ca>
2022-09-22 19:57:46 -0600

    added comments

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ test = 4
 test2 = 'Hello'
 
 try:
+    #code that may crash 
     test3 = test + test2
+    # specific error catch
 except TypeError:
     print('Can not add a string to a number')
+     # general error catch
 except:
     print('Error')
+    # a catch all wether error or not
 finally:
     print('okie dokie')
